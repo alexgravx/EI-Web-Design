de283dbaacbab60a85853972543c1aa4428b42e8
Paul-Aimery Preveraud de Vaumas <paul-aimery.preveraud-de-vaumas@student-cs.fr>
2023-06-02 14:51:43 +0200

    Dislaikelike

--- frontend/src/components/Movie/Movie.jsx
+++ frontend/src/components/Movie/Movie.jsx
@@ -30,64 +30,69 @@ function Movie({ movies }) {
     }
   };
 
+  const renderMovies = (movies) => {
+    return movies.map((film) => (
+      <div className="movies-posters" key={film.id}>
+        <img
+          className="movie-image"
+          alt="poster"
+          src={`https://image.tmdb.org/t/p/w1280${film.poster_path}`}
+          height="225"
+          width="150"
+          onClick={() => openPopup(film)}
+        />
+        {currentMovie && currentMovie.id === film.id && (
+          <div className={`popup ${currentMovie && 'active'}`}>
+            <div className="popup-content">
+              <img
+                className="popup-image"
+                alt="poster"
+                src={`https://image.tmdb.org/t/p/w1280${currentMovie.backdrop_path}`}
+              />
+              <div className="detail">
+                <div className="button-container">
+                  <div className="lk-buttons">
+                    <button className="like-button" onClick={handleLike}>
+                      <FontAwesomeIcon icon={faThumbsUp} />
+                    </button>
+                    <button className="dislike-button" onClick={handleDislike}>
+                      <FontAwesomeIcon icon={faThumbsDown} />
+                    </button>
+                  </div>
+                  <h1 className="titlepop">{currentMovie.original_title}</h1>
+                </div>
+                <h2 className="textvote">
+                  Recommandé à {currentMovie.vote_average * 10}%
+                </h2>
+                <h2 className="textOV">{currentMovie.overview}</h2>
+              </div>
+              <button className="button-close" onClick={closePopup}></button>
+            </div>
+          </div>
+        )}
+      </div>
+    ));
+  };
+
   return (
     <div className="films">
-      {movies.map((film) => (
-        <div className="movies-posters" key={film.id}>
-          <img
-            className="movie-image"
-            alt="poster"
-            src={`https://image.tmdb.org/t/p/w1280${film.poster_path}`}
-            height="225"
-            width="150"
-            onClick={() => openPopup(film)}
-          />
-          {currentMovie && currentMovie.id === film.id && (
-            <div className={`popup ${currentMovie && 'active'}`}>
-              <div className="popup-content">
-                <img
-                  className="popup-image"
-                  alt="poster"
-                  src={`https://image.tmdb.org/t/p/w1280${currentMovie.backdrop_path}`}
-                />
-                <div className="detail">
-                  <div className="button-container">
-                    <div className="lk-buttons">
-                      <button className="like-button" onClick={handleLike}>
-                        <FontAwesomeIcon icon={faThumbsUp} />
-                      </button>
-                      <button className="dislike-button" onClick={handleDislike}>
-                        <FontAwesomeIcon icon={faThumbsDown} />
-                      </button>
-                    </div>
-                    <h1 className="titlepop">{currentMovie.original_title}</h1>
-                  </div>
-                  <h2 className="textvote">
-                    Recommandé à {currentMovie.vote_average * 10}%
-                  </h2>
-                  <h2 className="textOV">{currentMovie.overview}</h2>
-                </div>
-                <button className="button-close" onClick={closePopup}></button>
-              </div>
-            </div>
-          )}
-        </div>
-      ))}
       <div className="liked-movies">
         <h3>Films aimés :</h3>
-        <ul>
-          {likedMovies.map((movie) => (
-            <li key={movie.id}>{movie.title}</li>
-          ))}
-        </ul>
+        <div className="liked-movies-posters">
+          {renderMovies(likedMovies)}
+        </div>
+      </div>
+      <div className="other-movies">
+        <h3>Autres films :</h3>
+        <div className="other-movies-posters">
+          {renderMovies(movies.filter((film) => !likedMovies.includes(film) && !dislikedMovies.includes(film)))}
+        </div>
       </div>
       <div className="disliked-movies">
         <h3>Films non aimés :</h3>
-        <ul>
-          {dislikedMovies.map((movie) => (
-            <li key={movie.id}>{movie.title}</li>
-          ))}
-        </ul>
+        <div className="disliked-movies-posters">
+          {renderMovies(dislikedMovies)}
+        </div>
       </div>
     </div>
   );
